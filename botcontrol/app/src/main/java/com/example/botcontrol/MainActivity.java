3c59ba70d7a7ce94756009d712ea935375caf7a3
Swisskill <wbrant@uwyo.edu>
2022-11-16 23:04:59 -0700

    large push; res changes mostly

--- botcontrol/app/src/main/java/com/example/botcontrol/MainActivity.java
+++ botcontrol/app/src/main/java/com/example/botcontrol/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.botcontrol;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-
+import android.widget.Toast;
+import android.view.LayoutInflater;
+import android.view.View;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +16,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Objects.requireNonNull(((MainActivity) this).getSupportActionBar()).hide();//get rid of toolbar
+        JoystickView joystick = (JoystickView) findViewById(R.id.joystickView);
+        joystick.setOnMoveListener(new JoystickView.OnMoveListener() {
+            @Override
+            public void onMove(int angle, int strength) {
+
+            }
+        });
+
     }
 }
