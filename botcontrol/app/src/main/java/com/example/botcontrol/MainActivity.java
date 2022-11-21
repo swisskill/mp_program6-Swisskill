065cb5b4bd7dc9e09d92bdede37628963c8b95e1
Swisskill <wbrant@uwyo.edu>
2022-11-21 15:11:14 -0700

    setting up networking

--- botcontrol/app/src/main/java/com/example/botcontrol/MainActivity.java
+++ botcontrol/app/src/main/java/com/example/botcontrol/MainActivity.java
@@ -13,6 +13,7 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,7 +23,8 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-
+    String IP;
+    String Player;
     @SuppressLint({"SetTextI18n", "ClickableViewAccessibility"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         String[] items = new String[]{"Demo", "Tank", "Scout"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
         dropdown.setAdapter(adapter);
-        Log.wtf("", dropdown.getSelectedItem().toString()); // this is how you get the item out of it baby
+        Log.d("", dropdown.getSelectedItem().toString()); // this is how you get the item out of it baby
         FloatingActionButton net = findViewById(R.id.net);
         //---------------------------------BUTTONS--------------------------------------------------
         fab.setOnTouchListener(new View.OnTouchListener() {
@@ -45,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
             public boolean onTouch(View view, MotionEvent motionEvent) {
                 if(motionEvent.getAction()==MotionEvent.ACTION_DOWN){
                     fab.setForeground(getResources().getDrawable(R.drawable.bpress));
-                    //Create thread for fire networking
+                    //Create thread for fire networking?
                     //will need to depend on bot probably. different functions or something
                 } else {
                     fab.setForeground(getResources().getDrawable(R.drawable.nopress));
@@ -53,44 +55,51 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
-
+        //---------------------------
         net.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 showDialog();
+                //probably now run a network thread that establishes a connection with the server
             }
         });
+        //---------------------------
         joystick.setOnMoveListener((angle, strength) -> {//lambda; cause why not
-            int quad;
+            int[] coord = {0,0}; //this scares me. how often is it getting reset. is this bad programming?
             if (strength !=0){
-                quad = quadrant(angle); //quad is important for x,y movement. actually, may just want to send out x,y pair, not quad
-            } else {quad = 0;}
-            textView.setText(angle + ":" + quad + ":" + strength);
+                coord = quadrant(angle); //this stuff seems sketch
+            } else {coord[0] = 0; coord[1] = 0;}
+            textView.setText(angle + ": " + coord[0] + " " + coord[1] );
         });
     }
     //---------------------------------METHODS------------------------------------------------------
-    public int quadrant(int angle){
-        int quad = 0;
-        if(337.5<angle || angle<22.5){quad = 1;}  //E ;that's so weird that I have to use an or. I wish i could just do #<angle<#
-        if(22.5<angle && angle<67.5){quad = 2;}   //NE
-        if(67.5<angle && angle<112.5){quad = 3;}  //N
-        if(112.5<angle && angle<157.5){quad = 4;} //NW
-        if(157.5<angle && angle<202.5){quad = 5;} //W
-        if(202.5<angle && angle<247.5){quad = 6;} //SW
-        if(247.5<angle && angle<292.5){quad = 7;} //S
-        if(292.5<angle && angle<337.5){quad = 8;} //SE
-        return quad;
+    public int[] quadrant(int angle){
+        int[] coord = {0,0}; //where 0th is x and 1st is y
+        if(337.5<angle || angle<22.5){coord[0] = 1; coord[1] = 0;}  //E ;that's so weird that I have to use an or. I wish i could just do #<angle<#
+        if(22.5<angle && angle<67.5){coord[0] = 1; coord[1] = 1;}   //NE
+        if(67.5<angle && angle<112.5){coord[0] = 0; coord[1] = 1;}  //N
+        if(112.5<angle && angle<157.5){coord[0] = -1; coord[1] = 1;} //NW
+        if(157.5<angle && angle<202.5){coord[0] = -1; coord[1] = 0;} //W
+        if(202.5<angle && angle<247.5){coord[0] = -1; coord[1] = -1;} //SW
+        if(247.5<angle && angle<292.5){coord[0] = 0; coord[1] = -1;} //S
+        if(292.5<angle && angle<337.5){coord[0] = 1; coord[1] = -1;} //SE
+        return coord;
     }
-
+    //---------------------------
     void showDialog() {
         LayoutInflater inflater = LayoutInflater.from(this);
         final View textenter = inflater.inflate(R.layout.dialog, null);
-//        final EditText ip = textenter.findViewById(R.id.ip); //this is how you grab info from the dialog box
+        final EditText ip = textenter.findViewById(R.id.ip); //does this need to be a "final"
+        final EditText player = textenter.findViewById(R.id.player);
+
         final AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(this, androidx.appcompat.R.style.Base_Theme_AppCompat_Dialog));
-        builder.setView(textenter).setTitle("Add");
-        builder.setPositiveButton("Connect", new DialogInterface.OnClickListener() {
+        builder.setView(textenter).setTitle("Connect");
+        builder.setPositiveButton("Submit", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
                 //probably something about submitting to the server
+                // or maybe we just set things and then keep networking in the ui code instead of a function call
+                IP = ip.getText().toString();
+                Player = player.getText().toString();
             }
         }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
@@ -100,7 +109,7 @@ public class MainActivity extends AppCompatActivity {
         builder.show();
     }
 
-    }
+}
 
 
 
---
+++ botcontrol/app/src/main/java/com/example/botcontrol/doNetwork.java
@@ -0,0 +1,13 @@
+package com.example.botcontrol;
+
+public class doNetwork implements Runnable{
+    //probably needs to have public variables for pieces from main that I need in this class
+    //in the main class, probably need *new doNetwork();* and *doNetwork.variable = variable;*
+    //syntax might not be perfect but the idea seems to make sense
+    //needed variables: IP and Players (i think)
+    @Override
+    public void run() {
+        //question of the day: should it be it's own file?
+        //I expect this to be rather similar to Jim's code
+    }
+}
